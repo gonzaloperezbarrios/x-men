87e67ee846b622370c68c66938e6c6954fb6c9a6
gonzalo <gonzalo@finaktiva.com>
2022-05-28 19:46:49 -0500

    se agrega comando start,para levantar el proyecto local

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import mutant from '@functions/mutant';
 const serverlessConfiguration: AWS = {
   service: 'dna-mutante-x',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
